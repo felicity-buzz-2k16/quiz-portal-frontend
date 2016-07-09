92cf33fa6012f7d10098bb78312d32c934cd7cb2
Anubhab Sen <anubhabsen@gmail.com>
2016-07-10 01:00:17 +0530

    Display username on nav bar (#6)
    Fixes #3

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,16 +1,20 @@
 var auth = {
   x: 2,
   token: localStorage.getItem('authToken'),
-  login(authToken) {
+  login(authToken, name, email) {
     // Set the authToken and reroute to index page
+    this.name = name
     this.token = authToken
     localStorage.authToken = authToken
+    localStorage.name = name
     if (typeof this.onLogin == 'function') this.onLogin()
   },
   logout() {
     // Unset the authToken and reroute to index page
     this.token = ''
+    this.name = ''
     localStorage.authToken = this.token
+    localStorage.name = this.name
     if (typeof this.onLogin == 'function') this.onLogout()
   },
 }
